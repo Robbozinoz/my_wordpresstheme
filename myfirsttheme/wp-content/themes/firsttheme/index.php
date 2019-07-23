e5c06ada9ae989acae7dad41ba414b8ac39b113d
Robbozinoz <Robbozinoz>
2019-07-23 14:45:15 +0800

    Functions file creation and lib initiation with helpers.php

--- myfirsttheme/wp-content/themes/firsttheme/index.php
+++ myfirsttheme/wp-content/themes/firsttheme/index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
 <?php if (have_posts()) { ?>
     <?php while (have_posts()) { ?>
         <?php the_post(); ?>
@@ -7,18 +6,14 @@
             <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_title() ?></a>
         </h2>
         <div>
-            Posted on
-            <a href="<?php echo get_permalink() ?>">
-                <time datetime="<?php echo get_the_date('c') ?>"><?php echo get_the_time(); ?></time>
-            </a>
-            By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>"><?php echo get_the_author(); ?></a>
+            <?php firsttheme_post_meta() ?>
         </div>
         <div>
             <?php the_excerpt() ?>
         </div>
-        <a href="<?php echo get_the_permalink() ?>" title="<?php the_title_attribute() ?>">
-            Read More <span class="u-screen-reader-text">About <?php the_title() ?></span>
-        </a>
+
+        <?php firsttheme_readmore_link(); ?>
+
     <?php } ?>
     <?php the_posts_pagination(); ?>
 <?php } else { ?>
