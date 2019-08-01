4f601ff4cd58f00aa746ab7da3ac2dc2f67422ec
Robbozinoz <Robbozinoz>
2019-08-01 14:07:36 +0800

    Helpper file update to include function escaping of attributes, url's and html

--- myfirsttheme/wp-content/themes/firsttheme/index.php
+++ myfirsttheme/wp-content/themes/firsttheme/index.php
@@ -6,12 +6,14 @@
             <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_title() ?></a>
         </h2>
         <div>
+            <!---See functions.php for setting oof which meta is extracted for the current post--->
             <?php firsttheme_post_meta() ?>
         </div>
         <div>
             <?php the_excerpt() ?>
         </div>
 
+        <!--This is to attach the current posts read more link - including span for accessibility-->
         <?php firsttheme_readmore_link(); ?>
 
     <?php } ?>
--- myfirsttheme/wp-content/themes/firsttheme/lib/helpers.php
+++ myfirsttheme/wp-content/themes/firsttheme/lib/helpers.php
@@ -1,15 +1,19 @@
 <?php
 
+//Site wide function for extarcting a variety of post meta
+//Enusre fnuction name is unique for submission to theme markets, avoiding conflicts
+//IMPORTANT-----URL/Htmla and Attributes escaped as standard
 function firsttheme_post_meta()
 {
     echo 'Posted on ';
-    echo '<a href="' . get_permalink() . '">';
-    echo '<time datetime="' . get_the_date('c') . '">' . get_the_time() . '</time></a>';
-    echo ' By <a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author() . '</a>';
+    echo '<a href="' . esc_url(get_permalink()) . '">';
+    echo '<time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time></a>';
+    echo ' By <a href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a>';
 }
 
+//Concatenated function for site wide Read more links
 function firsttheme_readmore_link()
 {
-    echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute(['echo' => false]) . '">';
-    echo 'Read More - <span class="u-screen-reader-text">About ' . get_the_title() . '</span></a>';
+    echo '<a href="' . esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
+    echo ' Read More  <span class="u-screen-reader-text">About: ' . get_the_title() . '</span></a>';
 }
