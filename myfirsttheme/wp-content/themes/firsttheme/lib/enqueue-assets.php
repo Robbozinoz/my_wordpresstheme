db3adc0876d3f6d4cbf181ab53a1177490a5b292
Robbozinoz <Robbozinoz>
2019-08-13 15:32:22 +0800

    Gulp update to include source maps, imagemin, copy & sass watch functions

---
+++ myfirsttheme/wp-content/themes/firsttheme/lib/enqueue-assets.php
@@ -0,0 +1,15 @@
+<?php
+
+function myfirsttheme_assets()
+{
+    wp_enqueue_style('myfirsttheme-stylesheet', get_template_directory_uri() . '/dist/assets/css/main.css', array(), '1.0.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'myfirsttheme_assets');
+
+function myfirsttheme_admin_assets()
+{
+    wp_enqueue_style('myfirsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+}
+
+add_action('admin_enqueue_scripts', 'myfirsttheme_admin_assets');
