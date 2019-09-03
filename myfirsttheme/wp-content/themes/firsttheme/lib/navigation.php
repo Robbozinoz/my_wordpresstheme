53c88794657fe1854563969a87162ca8324b589c
Robbozinoz <Robbozinoz>
2019-09-03 12:57:23 +0800

    Add Accessibility to menu including closing menus, and controlling the classes og the DOM

--- myfirsttheme/wp-content/themes/firsttheme/lib/navigation.php
+++ myfirsttheme/wp-content/themes/firsttheme/lib/navigation.php
@@ -1,5 +1,6 @@
 <?php
 
+//Register the menus
 function _themename_register_menus()
 {
     register_nav_menus(array(
@@ -10,15 +11,43 @@ function _themename_register_menus()
 
 add_action('init', '_themename_register_menus');
 
+//Accessibility funfilter for menu anchor tags
+function _themename_aria_hasdropdown($atts, $item, $args)
+{
+    if ($args->theme_location == 'main-menu') {
+        if (in_array('menu-item-has-children', $item->classes)) {
+            $atts['aria-haspopup'] = 'true';
+            $atts['aria-expanded'] = 'false';
+        }
+    }
+
+    return $atts;
+}
+
+add_filter('nav_menu_link_attributes', '_themename_aria_hasdropdown', 10, 3);
+
+//Add button element to dropdown tags to facilitate aria popup
+function _themename_submenu_button($dir = 'down', $title)
+{
+    $button = '<button class="menu-button">';
+    $button .= '<span class="u-screen-reader-text menu-button-show">' . sprintf(esc_html__('Show %s submenu', '_themename'), $title) . '</span>';
+    $button .= '<span aria-hidden="true" class="u-screen-reader-text menu-button-hide">' . sprintf(esc_html__('Hide %s submenu', '_themename'), $title) . '</span>';
+    $button .= '<i class="fa fa-angle-' . $dir . '" aria-hidden="true"></i>';
+    $button .= '</button>';
+
+    return $button;
+}
+
+
 //For adding icons to sub menus
 function _themename_dropdown_icon($title, $item, $args, $depth)
 {
     if ($args->theme_location == 'main-menu') {
         if (in_array('menu-item-has-children', $item->classes)) {
             if ($depth == 0) {
-                $title .= '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('down', $title);
             } else {
-                $title .= '<i class="fa fa-angle-right" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('right', $title);
             }
         }
     }
