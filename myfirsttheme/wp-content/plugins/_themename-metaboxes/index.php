7a5ef37d6489f6c136daf1d4643d6336152950cb
Robbozinoz <Robbozinoz>
2019-09-17 15:47:51 +0800

    Additon of new theme plugin

---
+++ myfirsttheme/wp-content/plugins/_themename-metaboxes/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: _themename _pluginname
+Plugin URI:
+Description: Adding Metaboxes for _themename
+Version: 1.0.0
+Author: John Robertson
+Author: https://www.myjamjar.com.au
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: _themename-_pluginname
+Domain Path: /languages
+*/
+
+//This stops unauthorised access to index.php file
+
+if (!defined('WPINC')) {
+    die;
+}
