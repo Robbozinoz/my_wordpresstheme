c023f2c901cdd2872ed2a321309b113ff7162167
Robbozinoz <Robbozinoz>
2019-10-02 13:27:04 +0800

    Update enqueue file

--- myfirsttheme/wp-content/themes/firsttheme/lib/enqueue-assets.php
+++ myfirsttheme/wp-content/themes/firsttheme/lib/enqueue-assets.php
@@ -15,8 +15,9 @@ function _themename_assets()
     wp_enqueue_script('_themename-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), '1.0.0', true);
 
     //Get the comments reply form from wp core
-    if (is_singular() && comments_open() && get_option('thread_comments'))
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
+    }
 }
 
 add_action('wp_enqueue_scripts', '_themename_assets');
